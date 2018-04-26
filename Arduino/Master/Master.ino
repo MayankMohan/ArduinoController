b81fd14697a076284506014c3fb8738e2c141fa2
Brandon Frale <bfrale2@uic.edu>
2018-04-26 18:18:41 -0500

    Setup basic code structure for Master device.

--- Arduino/Master/Master.ino
+++ Arduino/Master/Master.ino
@@ -1,53 +1,49 @@
-const int BTN[] = {8,9}; // Button pins
-const int interrupt = 2; //Interrupt Pin
-const int numAx = 2;
-const int axis[numAx] = {0,1}; // Analog axis pins
-const bool inv[numAx] = {false, false};
+#include <SoftwareSerial.h>
 
-int amin[numAx];
-int amax[numAx];
+// Serial port for left hand.
+// TODO - Insert correct ports
+SoftwareSerial leftPort(0, 0);
+// Serial port for right hand.
+SoftwareSerial rightPort(0, 0);
 
-byte packet[numAx];
+// Array of bytes that will be sent to VJoy.
+byte packet[8];
+// [0] - Joystick X-Axis
+// [1] - Joystick Y-Axis
+// [2-6] - 5 x Flex-Axis
+// [7] - Buttons
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(BTN[0], INPUT_PULLUP);
-  pinMode(BTN[1], INPUT_PULLUP);
-  pinMode(interrupt, INPUT);
+  // Open USB serial and wait for port to open.
   Serial.begin(9600);
-  for(int i = 0; i < numAx; i++){
-    amin[i] = analogRead(axis[i]);
-    amax[i] = amin[i];
+  while(!Serial) {
+    // Waiting for serial port to connect.
   }
-  attachInterrupt(digitalPinToInterrupt(interrupt), sendData, RISING);
+
+  // Start software serial ports.
+  leftPort.begin(9600);
+  leftPort.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  byte out = 0;
-  //Output analog Byte
-  for(int i = 0; i < numAx; i++){
-    int an = analogRead(axis[i]);
-    if(an < amin[i]){
-      amin[i] = an;
-    }
-    if(an > amax[i]){
-      amax[i] = an;
-    }
-    out = map(an, amin[i], amax[i], 0 + inv[i] * 100, 100 - inv[i] * 100);
-    packet[i] = out;
-  }
+  // Get data from each slave device.
+  pullLeftData();
+  pullRightData();
 
-  //Output buttons Byte
-  /*
-  out = (digitalRead(BTN[0]) == LOW) * 2 + (digitalRead(BTN[1]) == LOW);
-  Serial.write(out);
-  */
-  delay(5);
+  // Send data to VJoy
+  Serial.write(packet, 8);
+  
 }
 
-void sendData(){
-  for(int i = 0; i < numAx; i++){
-    Serial.write(packet[i]);
-  }
+void pullLeftData() {
+  // TODO:
+  // - Signal left
+  // - Pull left data and insert into byte array
 }
+
+void pullRightData() {
+  // TODO:
+  // - Signal right
+  // - Pull right data and insert into byte array
+}
+
