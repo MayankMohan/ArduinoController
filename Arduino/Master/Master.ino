f8dee65d68d60cf0875bbb2a21634dad03509e36
Brandon Frale <bfrale2@uic.edu>
2018-04-26 20:12:31 -0500

    Finished Master code

--- Arduino/Master/Master.ino
+++ Arduino/Master/Master.ino
@@ -1,17 +1,28 @@
 #include <SoftwareSerial.h>
 
-// Serial port for left hand.
-// TODO - Insert correct ports
-SoftwareSerial leftPort(0, 0);
-// Serial port for right hand.
-SoftwareSerial rightPort(0, 0);
+// Number of joystick axes.
+const int numJoyAxes = 2;
+// Number of hand axes (like a graph, not a hatchet).
+const int numHandAxes = 5;
+// Number of bytes required for all buttons on joystick.
+const int numButtonBytes = 1;
+// Total number of bytes.
+const int totalBytes = numJoyAxes + numHandAxes + numButtonBytes;
+
+// Pins used to send interrupt signal to slaves.
+// TODO - Set these to correct interrupt pins.
+int interruptControllerPin = 4;
+int interruptHandPin = 5;
+
+// Serial port for controller.
+// TODO - Set to correct pins.
+SoftwareSerial controllerPort(2, 1);
+// Serial port for glove.
+SoftwareSerial handPort(3, 1);
 
 // Array of bytes that will be sent to VJoy.
-byte packet[8];
-// [0] - Joystick X-Axis
-// [1] - Joystick Y-Axis
-// [2-6] - 5 x Flex-Axis
-// [7] - Buttons
+// Bytes order for data: [joystick axes][hand axes][button bytes].
+byte packet[totalBytes];
 
 void setup() {
   // Open USB serial and wait for port to open.
@@ -21,29 +32,52 @@ void setup() {
   }
 
   // Start software serial ports.
-  leftPort.begin(9600);
-  leftPort.begin(9600);
+  controllerPort.begin(9600);
+  handPort.begin(9600);
 }
 
 void loop() {
   // Get data from each slave device.
-  pullLeftData();
-  pullRightData();
+  pullControllerData();
+  pullHandData();
 
   // Send data to VJoy
-  Serial.write(packet, 8);
+  Serial.write(packet, totalBytes);
+}
+
+void pullControllerData() {
+  // Signal controller to send data. (1 millisecond delay should be more than enough time to detect interrupt)
+  digitalWrite(interruptControllerPin, HIGH);
+  delay(1);
+  digitalWrite(interruptControllerPin, LOW);
   
+  // Listen to controller data.
+  controllerPort.listen();
+  // Put controller data into packet.
+  for(int i = 0; controllerPort.available() > 0; i++) {
+    byte inByte = controllerPort.read();
+    if(i < numJoyAxes) {
+      packet[i] = inByte;
+    } else if(i < totalBytes) {
+      packet[(numJoyAxes + numHandAxes) + (i - numJoyAxes)] = inByte;
+    }
+  }
 }
 
-void pullLeftData() {
-  // TODO:
-  // - Signal left
-  // - Pull left data and insert into byte array
-}
-
-void pullRightData() {
-  // TODO:
-  // - Signal right
-  // - Pull right data and insert into byte array
+void pullHandData() {
+  // Signal hand to send data. (1 millisecond delay should be more than enough time to detect interrupt)
+  digitalWrite(interruptControllerPin, HIGH);
+  delay(1);
+  digitalWrite(interruptControllerPin, LOW);
+  
+  // Listen to hand data.
+  handPort.listen();
+  // Put hand data into packet.
+  for(int i = numJoyAxes; handPort.available() > 0; i++) {
+    byte inByte = handPort.read();
+    if(i < numHandAxes + numJoyAxes) {
+      packet[i] = inByte;
+    }
+  }
 }
 
